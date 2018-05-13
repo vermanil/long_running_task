928d7948ce32a4f885051b8f4adb14808f0ec341
vermanil <anilrajverma1996@mail.com>
2018-05-13 14:58:30 +0530

    celery config done

--- app.py
+++ app.py
@@ -1,31 +1,19 @@
 from flask import Flask, render_template
 from os import environ
-from celery import Celery, shared_task
-# from tasks import dine
-import request
+from celery import Celery
 
-def make_celery(app):
+def configure_celery(app):
     # set redis url vars
     app.config['CELERY_BROKER_URL'] = environ.get('REDIS_URL', 'redis://localhost:6379/0')
     app.config['CELERY_RESULT_BACKEND'] = app.config['CELERY_BROKER_URL']
-    app.config['CELERY_IMPORTS'] = ("tasks",)
-    # create context tasks in celery
-    celery = Celery(app.import_name, broker=app.config['CELERY_BROKER_URL'])
+
+    celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
     celery.conf.update(app.config)
-    TaskBase = celery.Task
-
-    class ContextTask(TaskBase):
-        abstract = True
-        def __call__(self, *args, **kwargs):
-            with app.app_context():
-                return TaskBase.__call__(self, *args, **kwargs)
-
-    celery.Task = ContextTask
-
+    
     return celery
 
 app = Flask(__name__)
-celery = make_celery(app)
+celery = configure_celery(app)
 
 @app.route('/')
 def landing_page():
@@ -33,8 +21,7 @@ def landing_page():
 
 @app.route('/upload-file')
 def upload_file(*args, **kwargs):
-    # dine.apply_async(*args, **kwargs)
-    hello_task.apply_async(*args, **kwargs)
+    hello_task.delay(*args, **kwargs)
     return render_template('fileUpload.html')
 
 @celery.task
--- tasks.py
+++ tasks.py
@@ -1,6 +1,2 @@
-# from celery.decorators import shared_task
 
-# @shared_task
-# def dine():
-# 	print "dhbhfdbfhgbfhdj"
 
